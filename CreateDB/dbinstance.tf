1bf0a65a8f156c498bd47dcc78e38fca2673e0f9
Vijay <45725513+vijaykrishna771@users.noreply.github.com>
2022-02-11 14:21:09 +0530

    Update dbinstance.tf

--- CreateDB/dbinstance.tf
+++ CreateDB/dbinstance.tf
@@ -4,8 +4,8 @@ provider "google" {
 project = "${var.project}"
 region = "${var.region}"
 }
-resource "google_sql_database_instance" "testdb" {
-name = "test-db"
+resource "google_sql_database_instance" "test-db-1" {
+name = "test-db-1"
 database_version = "MYSQL_5_7"
 region = "${var.region}"
 settings {
@@ -14,11 +14,11 @@ tier = "db-n1-standard-2"
 }
 resource "google_sql_database" "database" {
 name = "database_name"
-instance = "${google_sql_database_instance.testdb.name}"
+instance = "${google_sql_database_instance.test-db-1.name}"
 }
 
 resource "google_sql_user" "users" {
 name = "harish"
-instance = "${google_sql_database_instance.testdb.name}"
+instance = "${google_sql_database_instance.test-db-1.name}"
 password = "password"
 }
